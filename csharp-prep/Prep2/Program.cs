5615f318fa143a73f2cea6d89554c129f6466ec3
Sabra Crowther <smi17152@byui.edu>
2024-09-25 22:43:54 -0600

    finished code to display grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage. ");
+        string userInput = Console.ReadLine();
+        
+        int gradePerc = int.Parse(userInput);
+        string letter;
+
+        if (gradePerc <= 100 && gradePerc >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePerc < 90 && gradePerc >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePerc < 80 && gradePerc >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePerc < 70 && gradePerc >= 60)
+        {
+            letter = "D";
+        }
+        else if (gradePerc < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Grade: {letter}");
+
+        if (gradePerc >= 70)
+        {
+            Console.WriteLine("Congradulations! You Passed!");
+        }
+        else
+        {
+            Console.WriteLine("You'll do better next time!");
+        }
+        
     }
 }
