a293d7936b924d3b51feadc051ff380713a41fca
Sabra Crowther <smi17152@byui.edu>
2024-09-28 21:07:52 -0600

    finished guess number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magNum = randomGenerator.Next(1, 101);
+        Console.WriteLine("Guess the magic number. ");
+        int guess;
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+
+            if (guess < magNum)
+            {
+                Console.WriteLine("Higher ");
+            }
+            else if (guess > magNum)
+            {
+                Console.WriteLine("Lower ");
+            }
+            else
+            {
+                Console.Write("You guessed it!");
+            }
+
+        }while (guess != magNum);
+        
     }
 }
