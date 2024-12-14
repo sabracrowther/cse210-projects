330c5d7ef19420f50955937352a997183ec13d3f
Sabra Crowther <smi17152@byui.edu>
2024-12-14 12:30:46 -0700

    pushing test

--- final/FinalProject/Pet.cs
+++ final/FinalProject/Pet.cs
@@ -3,52 +3,103 @@ using System.ComponentModel;
 
 public class Pet
 {
-    private string _name = "Fifi";
-    private string _petType = "dog";
-    private string _color = "brown";
-     
+    protected string _name;
+    protected string _petType;
+    protected string _color;
+    protected string _description;
+    //set Lists to private so they can't be changed
+    private List<string> _movements;
+    private List<string> _interactions;
+    private List<string> _bodyFeatures;
+    private List<string> _vocalizations;
 
-    public Pet()
-    {
-        
+    public Pet() //default constructor
+    {        
+        //minimum for any type of animal
+        _movements = new List<string> {"eat", "sleep"};
+        _interactions = new List<string> {"watch"};
+        _bodyFeatures = new List<string> {"eyes", "head"};
+        _vocalizations = new List<string> {""};  //none is default        
     }
 
-    public string GetColor{
+    public string Color{
         get => _color;
     }
 
-    public string GetPetype
+    public string Petype
     {
         get => _petType;
     }
 
-    public string GetName
+    public string Name
     {
         get => _name;
-    }    
+    } 
 
-    public string SetPetType(string petType)
+    public List<string> Movements
     {
-        _petType = petType;
-        return _petType;
-    }   
-
-
-    public void GetPet()
-    {
-        Console.WriteLine($"The pet you chose is {_name}, it is a {_petType}, and the color is {_color}.");
+        get => _movements;
     }
 
-    public void CreateNewPet()
+    public List<string> Interactions
     {
-        Console.Write("What type of pet would you like to create? Dog, Cat, Bird, Hamster, Fish ");
-        _petType = Console.ReadLine();
-        Console.Write("What color is your pet? ");
-        _color = Console.ReadLine();
-        Console.Write("What is the name of your pet? ");
-        _name = Console.ReadLine();
-        Console.WriteLine($"You have created a {_color} {_petType}, named {_name}.");
+        get => _interactions;
     }
 
+    public List<string> BodyFeatures
+    {
+        get => _bodyFeatures;
+    }
+
+    public List<string> Vocalizations
+    {
+        get => _vocalizations;
+    }
+
+    protected void AddMovements(string[] movementStrArray)
+    {
+        _movements.AddRange(movementStrArray.ToList());
+    }
+
+    protected void AddMovements(List<string> movementList)
+    {
+        _movements.AddRange(movementList);
+    }
+
+    protected void AddInteractions(string[] strArray)
+    {
+        _interactions.AddRange(strArray.ToList());
+    }
+
+    protected void AddInteractions(List<string> strList)
+    {
+        _interactions.AddRange(strList);
+    }
+
+     protected void AddBodyFeatures(string[] strArray)
+    {
+        _bodyFeatures.AddRange(strArray.ToList());
+    }
+
+    protected void AddBodyFeatures(List<string> strList)
+    {
+        _bodyFeatures.AddRange(strList);
+    }
+
+    protected void ReplaceVocalizations(string[] strArray)
+    {
+        _vocalizations.AddRange(strArray.ToList());
+    }
+
+    protected void ReplaceVocalizations(List<string> strList)
+    {
+        _vocalizations.AddRange(strList);
+    }
+
+   
+    public virtual string GetPetInfo()
+    {
+        return $"Your pet's name is {_name}, it is a {_petType}, and it is {_color}.";
+    }
 
 }
